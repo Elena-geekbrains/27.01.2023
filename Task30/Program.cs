b7c3505d5fd16794d75e1221cafc416e0620a600
Elena <0078122446925@mail.ru>
2023-01-27 14:44:15 +0300

    Added Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 30: Напишите программу, которая
+выводит массив из 8 элементов, заполненный
+нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+int[] ArrayGeneration (int size,int min, int max)
+{
+    int [] arr = new int [size];
+    Random rnd =new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i]=rnd.Next(min,max+1);
+    }
+    return arr;
+}
+
+void PrintArray (int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int [] array = ArrayGeneration(8,0,1);
+PrintArray(array);
