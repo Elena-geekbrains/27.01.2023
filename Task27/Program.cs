63839cfae9ee88100403184768a87d8b18415d31
Elena <0078122446925@mail.ru>
2023-01-27 22:39:32 +0300

    Added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 27
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int CountDigit(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int countDigit = CountDigit(number);
+int sum = 0;
+for (int i = 0; i < countDigit; i++)
+{
+    sum += number % 10;
+    number /= 10;
+}
+Console.WriteLine($"Сумма цифр в задвнном числе: {sum}.");
