fda5ee6095d1d9a346c109620905501d764492d3
Elena <0078122446925@mail.ru>
2023-01-27 16:15:08 +0300

    Added Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 29
+Напишите программу, которая задаёт массив из 8 элементовb выводит их на экран.
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33] */
+
+int[] ArrayGeneration(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+    }
+    return arr;
+}
+
+int[] array = ArrayGeneration(8);
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == array.Length - 1) Console.Write($"{array[i]} -> ");
+    else Console.Write($"{array[i]}, ");
+}
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == array.Length - 1) Console.Write($"{array[i]} ");
+    else Console.Write($"{array[i]}, ");
+}
+Console.Write("]");
