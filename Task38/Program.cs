47f205ca5b21aa191f9d0b1de1b7d9b8be6d1299
Elena <0078122446925@mail.ru>
2023-01-30 14:52:28 +0300

    Added Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задача 38
+Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2 */
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round((rnd.NextDouble() * (max - min) + min), 2);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}; ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("] ");
+}
+
+double GetMaxMinDif(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+        else if (array[i] < min) min = array[i];
+    }
+    double dif = max - min;
+    double result = Math.Round(dif, 2);
+    return result;
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+double[] arr = CreateArrayRndDouble(size, 0, 10);
+PrintArray(arr);
+
+double difMaxMin = GetMaxMinDif(arr);
+Console.WriteLine($"-> {difMaxMin}");
