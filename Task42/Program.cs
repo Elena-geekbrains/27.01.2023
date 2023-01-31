156e90e5a45384f831211c8c8d62a59fb0ba1dc9
Elena <0078122446925@mail.ru>
2023-01-31 18:30:48 +0300

    Фввув Ефыл42

---
+++ Task42/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 42
+Преобразовать десятичное в двоичное
+45 -> 101101
+3 -> 11*/
+
+Console.WriteLine("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int BinaryNumber(int num)
+{
+    int res = 0;
+    int count = 1;
+    while (num > 0)
+    {
+        res += num % 2 * count;
+        num /= 2;
+        count *= 10;
+    }
+    return res;
+}
+
+int result = BinaryNumber(number);
+Console.WriteLine(result);
