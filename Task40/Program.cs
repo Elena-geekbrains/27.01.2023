2d52aa18020df97295f5e05bcb9ff608f9427f99
Elena <0078122446925@mail.ru>
2023-01-31 18:29:34 +0300

    Added Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 40
+Пинимает 3 числа, может ли существовать трекгольник с такими сторонами. */
+
+Console.WriteLine("Введите первую сторону трекгольника: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите вторую сторону трекгольника: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третью сторону трекгольника: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+bool Triangle(int num1, int num2, int num3)
+{
+    return num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2;
+}
+
+bool result = Triangle(number1, number2, number3);
+Console.WriteLine(result
+? "да, трекгольник с такими сторонами существует"
+: "нет, трекгольника с такими сторонами не существует");
