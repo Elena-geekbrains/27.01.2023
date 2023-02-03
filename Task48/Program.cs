f64ec54312a563838ef314e014780841a1bf67fe
Elena <0078122446925@mail.ru>
2023-02-03 22:57:41 +0300

    Added Task49

---
+++ Task48/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 48
+Массив m на n. Amn=m+n
+m=3, n=4.
+0123
+1234
+2345 */
+
+int[,] CreateMatrixRndItn(int rows, int colums)
+{
+    int[,] matrix = new int[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i+j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 4} | ");
+            else Console.Write($"{matrix[i,j], 4}");
+        }
+       Console.WriteLine(" | ");
+    }
+}
+
+int[,]array2D = CreateMatrixRndItn(3,4);
+PrintMatrix(array2D);
