0d60b9de3b862846efac54809a5a635f85e44d1a
Elena <0078122446925@mail.ru>
2023-02-11 12:26:41 +0300

    Added Task63

---
+++ Task63/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 63
+Задайте значение N. Вывести все натуральные число в промежутке от 1 до N. */
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
