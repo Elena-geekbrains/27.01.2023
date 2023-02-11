87b7c8265203c28e851626759f4a19ce1be50c35
Elena <0078122446925@mail.ru>
2023-02-11 12:40:22 +0300

    Added Task65

---
+++ Task65/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 63
+Задайте значение N,M. Вывести все натуральные число в промежутке от M до N. */
+
+Console.WriteLine("Введите натуральное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(numberM, numberN);
+
+void NaturalNumbers(int num1, int num2)
+{
+    Console.Write($"{num1} ");
+    if (num1 == num2) return;
+    if (num1 > num2) NaturalNumbers(num1 - 1, num2);
+    else NaturalNumbers(num1 + 1, num2);
+}
