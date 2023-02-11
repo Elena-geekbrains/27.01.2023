90795ebdc431da55901351ba96bdc69f181161ba
Elena <0078122446925@mail.ru>
2023-02-11 13:11:50 +0300

    Added Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 67
+Принимаем число и возвращаем сумму его цифр. */
+
+int SumDigit(int num)
+{
+    if (num == 0) return 0;
+    else return num % 10 + SumDigit(num / 10);
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigit = SumDigit(number);
+Console.WriteLine(sumDigit);
