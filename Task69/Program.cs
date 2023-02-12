91c4ec98fbb8b3053f319b0ec36d82cda15776bb
Elena <0078122446925@mail.ru>
2023-02-12 05:44:04 +0300

    Added Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 69
+Принимаем А и В, возвращаем А в натуральной степени В с помощью рекурсии.*/
+
+
+int Powering(int num1, int num2)
+{
+    return num2 == 0 ? 1 : num1 = num1 * Powering(num1, num2 - 1);
+}
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int powering = Powering(numberA, numberB);
+Console.WriteLine(powering);
